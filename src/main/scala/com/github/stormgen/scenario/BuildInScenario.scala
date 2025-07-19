2c5b484979e39a3df238ee891d790f829687b995
Yue Li <yueli@mac.home>
2025-07-19 12:52:04 +0200

    Add build in test scenario

--- src/main/scala/com/github/stormgen/scenario/BuildInScenario.scala
+++ src/main/scala/com/github/stormgen/scenario/BuildInScenario.scala
@@ -1,13 +1,14 @@
 package com.github.stormgen.scenario
 
-import fs2.kafka.KafkaSerializer
+import fs2.kafka.{KafkaSerializer, KeySerializer, ValueSerializer}
 
-import scala.concurrent.duration.DurationInt
+import scala.concurrent.duration.{DurationInt, FiniteDuration}
 import scala.util.Random
 
 object BuildInScenario {
-  def smokeTest[K, V](keySerializer: KafkaSerializer[K], valueSerializer: KafkaSerializer[V], bootstrapServer: String, topic: String, numberPerSecond: Int) = {
-    val duration = Random.between(5, 15)
+  def smokeTest[K, V](keySerializer: KafkaSerializer[K], valueSerializer: KafkaSerializer[V], bootstrapServer: String, topic: String, numberPerSecond: Int): ScenarioSettings[K, V] = {
+    val duration = Random.between(10, 30)
+
     ScenarioSettings
       .create(keySerializer, valueSerializer)
       .withBootstrapServers(bootstrapServer)
@@ -15,8 +16,9 @@ object BuildInScenario {
       .withPhase(Phase(duration.minutes, Rate(numberPerSecond, perDuration = 1.second)))
   }
 
-  def averageLoadTest[K, V](keySerializer: KafkaSerializer[K], valueSerializer: KafkaSerializer[V], bootstrapServer: String, topic: String, numberPerSecond: Int) = {
-    val duration = Random.between(30, 120)
+  def averageLoadTest[K, V](keySerializer: KafkaSerializer[K], valueSerializer: KafkaSerializer[V], bootstrapServer: String, topic: String, numberPerSecond: Int): ScenarioSettings[K, V] = {
+    val duration = Random.between(10, 20)
+
     ScenarioSettings
       .create(keySerializer, valueSerializer)
       .withBootstrapServers(bootstrapServer)
@@ -24,11 +26,52 @@ object BuildInScenario {
       .withPhase(Phase(duration.minutes, Rate(numberPerSecond, perDuration = 1.second)))
   }
 
-  def spikeTest = ???
+  def soakTest[K, V](keySerializer: KafkaSerializer[K], valueSerializer: KafkaSerializer[V], bootstrapServer: String, topic: String, numberPerSecond: Int): ScenarioSettings[K, V] = {
+    val duration = Random.between(60, 90)
 
-  def stressTest = ???
+    ScenarioSettings
+      .create(keySerializer, valueSerializer)
+      .withBootstrapServers(bootstrapServer)
+      .withTopic(topic)
+      .withPhase(Phase(duration.minutes, Rate(numberPerSecond, 1.second)))
+  }
 
-  def breakpointTest = ???
+  def stressTest[K, V](keySerializer: KafkaSerializer[K], valueSerializer: KafkaSerializer[V], bootstrapServer: String, topic: String, numberPerSecond: Int): ScenarioSettings[K, V] = {
+    val duration = Random.between(10, 20)
 
-  def soakTest = ???
+    ScenarioSettings
+      .create(keySerializer, valueSerializer)
+      .withBootstrapServers(bootstrapServer)
+      .withTopic(topic)
+      .withPhase(Phase(duration.minutes, Rate(numberPerSecond, perDuration = 1.second)))
+  }
+
+  def spikeTest[K, V](keySerializer: KafkaSerializer[K], valueSerializer: KafkaSerializer[V], bootstrapServer: String, topic: String, lowLoadNumberPerSecond: Int, highLoadNumberPerSecond: Int): ScenarioSettings[K, V] = {
+    val lowDuration = Random.between(30, 90)
+    val highDuration = Random.between(30, 60)
+
+    ScenarioSettings
+      .create(keySerializer, valueSerializer)
+      .withBootstrapServers(bootstrapServer)
+      .withTopic(topic)
+      .withPhase(Phase(lowDuration.seconds, Rate(lowLoadNumberPerSecond, 1.second)))
+      .withPhase(Phase(highDuration.seconds, Rate(highLoadNumberPerSecond, 1.second)))
+  }
+
+  def breakpointTest[K, V](keySerializer: KafkaSerializer[K], valueSerializer: KafkaSerializer[V], bootstrapServer: String, topic: String, stepDuration: FiniteDuration, stepNumber: Int, totalDuration: FiniteDuration): ScenarioSettings[K, V] = {
+    val numberOfRound = totalDuration.toSeconds / stepDuration.toSeconds
+    val phases: Seq[Phase] = (1 to numberOfRound).foldLeft(Seq.empty[Phase]) { (phases, _) =>
+      val previousPhaseNumber = phases.lastOption.map(_.rate.number).getOrElse(0)
+      val nextPhase = Phase(stepDuration, Rate(previousPhaseNumber + stepNumber, 1.second))
+      phases :+ nextPhase
+    }
+
+    ScenarioSettings
+      .create(keySerializer, valueSerializer)
+      .withBootstrapServers(bootstrapServer)
+      .withTopic(topic)
+      .withPhases(phases)
+  }
+
+
 }
